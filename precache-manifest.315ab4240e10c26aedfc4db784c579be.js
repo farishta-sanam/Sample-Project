9f22cb771719449230fe6e15f35193e422c056db
farishta-sanam <farishtasanam@gmail.com>
2023-03-20 02:26:13 +0530

    Updates

---
+++ precache-manifest.315ab4240e10c26aedfc4db784c579be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0e7b27151448a57cd00",
+    "url": "/Sample-Project/static/css/main.370a09b6.chunk.css"
+  },
+  {
+    "revision": "e0e7b27151448a57cd00",
+    "url": "/Sample-Project/static/js/main.e0e7b271.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/Sample-Project/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/Sample-Project/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "3aaf2bb650361e526b11",
+    "url": "/Sample-Project/static/js/runtime~main.3aaf2bb6.js"
+  },
+  {
+    "revision": "7539f7beda316ecddd55a30581cffd3c",
+    "url": "/Sample-Project/index.html"
+  }
+];
